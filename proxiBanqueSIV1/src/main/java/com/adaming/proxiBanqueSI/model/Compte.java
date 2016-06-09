cbead5505f21e07c5363f84558add4f9c7b42c68
INTI-0213 <INTI-0213@INTI0213>
2016-06-09 14:39:24 +0200

    modif Compte, hibernate annotations

--- proxiBanqueSIV1/src/main/java/com/adaming/proxiBanqueSI/model/Compte.java
+++ proxiBanqueSIV1/src/main/java/com/adaming/proxiBanqueSI/model/Compte.java
@@ -8,10 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 /**
  * Classe abstraite de tous les types de compte.
@@ -19,7 +21,8 @@ import javax.persistence.Table;
  *
  */
 @Entity(name="compte")
-@Table(name="Compte")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@Table(name="comptes")
 public abstract class Compte implements Serializable {
 
 	private static final long serialVersionUID = 1L;
